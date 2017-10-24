dac9e0ce41e117a29e1d4d37b8219e1a2408fecb
Derek <dare.olson@gmail.com>
2017-10-24 16:35:04 -0700

    adding day, date, Month and year

--- app.js
+++ app.js
@@ -170,7 +170,8 @@ var UIController = (function() {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercentageLabel: '.item__percentage'
+		expensesPercentageLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
 
@@ -291,11 +292,27 @@ var UIController = (function() {
 					current.textContent = percentages[index] + '%';
 				} else {
 					current.textContent = '---';
-				}
-				
-
+				}	
 			});
+		},
 
+		displayMonth: function(){
+			var now, year, month, months, days;
+
+			now = new Date();
+
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+			days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+			month = now.getMonth();
+
+			day = now.getDay();
+
+			date = now.getDate();
+
+			year = now.getFullYear();
+			document.querySelector(DOMstrings.dateLabel).textContent = days[day] + ' ' + months[month] + ' ' + date + ', ' + year;
 		},
 
 		getDOMstrings: function() {
@@ -410,6 +427,7 @@ var UIController = (function() {
 	return {
 		init: function(){
 			console.log('Application has started');
+			UICntrl.displayMonth();
 			UICntrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
